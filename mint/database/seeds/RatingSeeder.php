3918d98ef40e0aa76ae4369d2d0fc5d7361dcf5c
Dorra <dorra.hbaieb@gmail.com>
2020-08-21 16:17:03 +0200

    seed rating

--- mint/database/seeds/RatingSeeder.php
+++ mint/database/seeds/RatingSeeder.php
@@ -12,10 +12,34 @@ class RatingSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => Str::random(10),
-            'email' => Str::random(10).'@gmail.com',
-            'password' => Hash::make('password'),
-        ]);
+        $ratings = array(
+            array('writer_id' => '1',
+            'target_id' => '2',
+            'score' => '5',
+            'comment' => 'this is the first message'),
+
+            array('writer_id' => '1',
+            'target_id' => '2',
+            'score' => '3',
+            'comment' => 'this is the second message'),
+
+            array('writer_id' => '1',
+            'target_id' => '2',
+            'score' => '2',
+            'comment' => 'this is the third message'),
+            
+        );
+        foreach ($ratings as $rating) {
+
+            DB::table('ratings')->insert([
+
+                'writer_id' => $rating['writer_id'],
+                'target_id' => $rating['price'],
+                'score' => $rating['author_id'],
+                'comment' => $rating['comment'],
+            
+                ]);    
+        }
+        
     }
 }
