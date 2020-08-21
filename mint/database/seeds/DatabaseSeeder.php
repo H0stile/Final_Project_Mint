de20803a0363125177ecb2e0464b96ad84814d2b
Matt <matthieu.barbie@gmail.com>
2020-08-21 16:02:48 +0200

    Update DatabaseSeeder.php

--- mint/database/seeds/DatabaseSeeder.php
+++ mint/database/seeds/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UserSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(SkillSeeder::class);
-        // $this->call(MessageSeeder::class);
+        $this->call(MessageSeeder::class);
     }
 }
