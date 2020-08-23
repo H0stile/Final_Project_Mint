a0d7779350f4deddf4be341516111106356fd865
MattRM2 <matthieu.barbie@gmail.com>
2020-08-23 18:04:04 +0200

    Update mentorAllConnection.blade.php
    - Upadte to have a simple jquery confirmation to delete at the moment

--- mint/resources/views/mentorAllConnection.blade.php
+++ mint/resources/views/mentorAllConnection.blade.php
@@ -43,24 +43,24 @@
                 })
             });
             //? Button to break the connection
-            //TODO Add confirm function jquery and if yes run the AJAX call
-
             $("button[name='getIdCollab']").click(function (event) {
                 event.preventDefault();
-                routeUrl = "{{url('')}}/disconnect/" + $(this).val();
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-                    }
-                });
-                $.ajax({
-                    url: routeUrl,
-                    method: 'GET',
-                    dataType: 'json',
-                    success: function (result) {
-                        location.reload();
-                    }
-                })
+                if(confirm("Are you sure to disconnect from this mentee ?")){
+                    routeUrl = "{{url('')}}/disconnect/" + $(this).val();
+                    $.ajaxSetup({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+                        }
+                    });
+                    $.ajax({
+                        url: routeUrl,
+                        method: 'GET',
+                        dataType: 'json',
+                        success: function (result) {
+                            location.reload();
+                        }
+                    })
+                }
             });
         });
     </script>
