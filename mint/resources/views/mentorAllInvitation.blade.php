4f5ba765f81a4a671545c6d2dd3123321a0657ac
MattRM2 <matthieu.barbie@gmail.com>
2020-08-24 16:24:16 +0200

    Update mentorAllInvitation.blade.php

--- mint/resources/views/mentorAllInvitation.blade.php
+++ mint/resources/views/mentorAllInvitation.blade.php
@@ -1,12 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mentor all invitation page</title>
-</head>
-<body>
-    <section class="invitCard">
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row">
+        @if (session('status'))
+        <div class="card green darken-1">
+            <div class="card-content white-text">
+                {{ session('status') }}
+            </div>
+        </div>
+        @endif
+        <section class="invitCard">
         @foreach($menteeRequests as $menteeRequest)
             <div>
                 <img src="{{asset('img/')}}/{{$menteeRequest->mentee->profile_image}}" style="width:60px">
@@ -20,17 +23,43 @@
             <hr>
         @endforeach
     </section>
-    <!-- IMPORT JQUERY -->
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <!-- MY SCRIPT -->
-    <!-- TODO ASK Simon for this -->
-    <!-- <script src="{{url('/resources/js/mentorAllConnection.js')}}"></script> -->
-    <script>
-        $(document).ready(function () {
-            //? Button to go to profile
-            $("button[name='getIdMentee']").click(function (event) {
-                event.preventDefault();
-                routeUrl = "{{url('')}}/profile/" + $(this).val();
+    </div>
+</div>
+@endsection
+
+<!-- IMPORT JQUERY -->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<!-- MY SCRIPT -->
+<!-- TODO ASK Simon for this -->
+<!-- <script src="{{url('/resources/js/mentorAllConnection.js')}}"></script> -->
+<script>
+    $(document).ready(function () {
+        //? Call Ajax at loading
+
+
+        //? Button to go to profile
+        $("button[name='getIdMentee']").click(function (event) {
+            event.preventDefault();
+            routeUrl = "{{url('')}}/profile/" + $(this).val();
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+                }
+            });
+            $.ajax({
+                url: routeUrl,
+                method: 'GET',
+                dataType: 'json',
+                success: function (result) {
+
+                }
+            })
+        });
+        //? Decline invitation
+        $("button[name='getIdCollabDec']").click(function (event) {
+            event.preventDefault();
+            if(confirm("Are you sure to decline this invitation ?")){
+                routeUrl = "{{url('')}}/mentoraidecline/" + $(this).val();
                 $.ajaxSetup({
                     headers: {
                         'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
@@ -41,51 +70,31 @@
                     method: 'GET',
                     dataType: 'json',
                     success: function (result) {
-
+                        location.reload();
                     }
                 })
-            });
-            //? Decline invitation
-            $("button[name='getIdCollabDec']").click(function (event) {
-                event.preventDefault();
-                if(confirm("Are you sure to decline this invitation ?")){
-                    routeUrl = "{{url('')}}/mentoraidecline/" + $(this).val();
-                    $.ajaxSetup({
-                        headers: {
-                            'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-                        }
-                    });
-                    $.ajax({
-                        url: routeUrl,
-                        method: 'GET',
-                        dataType: 'json',
-                        success: function (result) {
-                            location.reload();
-                        }
-                    })
-                }
-            });
+            }
         });
-            //? Accept the invitation
-            $("button[name='getIdCollabAcc']").click(function (event) {
-                event.preventDefault();
-                if(confirm("Are you sure to accept this invitation ?")){
-                    routeUrl = "{{url('')}}/mentoraiaccept/" + $(this).val();
-                    $.ajaxSetup({
-                        headers: {
-                            'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-                        }
-                    });
-                    $.ajax({
-                        url: routeUrl,
-                        method: 'GET',
-                        dataType: 'json',
-                        success: function (result) {
-                            location.reload();
-                        }
-                    })
-                }
-            });
-    </script>
-</body>
-</html>
+        //? Accept the invitation
+        $("button[name='getIdCollabAcc']").click(function (event) {
+            event.preventDefault();
+            if(confirm("Are you sure to accept this invitation ?")){
+                routeUrl = "{{url('')}}/mentoraiaccept/" + $(this).val();
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+                    }
+                });
+                $.ajax({
+                    url: routeUrl,
+                    method: 'GET',
+                    dataType: 'json',
+                    success: function (result) {
+                        location.reload();
+                    }
+                })
+            }
+        });
+    });
+</script>
+
