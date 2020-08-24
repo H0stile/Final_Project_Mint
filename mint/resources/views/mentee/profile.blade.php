38437c24c9e0ba7b3ee28dd558593c484f85011d
AnnaKhitko <33467618+AnnaKhitko@users.noreply.github.com>
2020-08-24 21:46:29 +0200

    Extend base layout.

--- mint/resources/views/mentee/profile.blade.php
+++ mint/resources/views/mentee/profile.blade.php
@@ -1,124 +1,114 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
+@section('content')
+<img src="{{asset('img/')}}/{{$profile->profile_image}}" style="width:60px">
+<h1>{{$profile->getFullName()}}</h1>
+<h3>You are a {{$profile->type}}</h3>
+@if(Auth::check())
+<p>Hello {{Auth::user()->getFullName()}}</p>
+@endif
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<hr>
+<h3>Pitch:</h3>
+<p>{{$profile->pitch}}</p>
+<hr>
+<h3>Review:</h3>
+@foreach($profile->receiveRatings as $rating)
+<p>{{$rating->writer->getFullName()}}</p>
+<p>{{$rating->score}}</p>
+<p>{{$rating->comment}}</p>
+@endforeach
+<hr>
 
-<body>
-    <img src="{{asset('img/')}}/{{$profile->profile_image}}" style="width:60px">
-    <h1>{{$profile->getFullName()}}</h1>
-    <h3>You are a {{$profile->type}}</h3>
-    @if(Auth::check())
-    <p>Hello {{Auth::user()->getFullName()}}</p>
-    @endif
-
-    <hr>
-    <h3>Pitch:</h3>
-    <p>{{$profile->pitch}}</p>
-    <hr>
-    <h3>Review:</h3>
-    @foreach($profile->receiveRatings as $rating)
-    <p>{{$rating->writer->getFullName()}}</p>
-    <p>{{$rating->score}}</p>
-    <p>{{$rating->comment}}</p>
-    @endforeach
-    <hr>
-
-    <form id="form" action="">
-        @csrf
-        <label id="label" for="">Write a message</label>
-        <br>
-        <textarea name="" id="textArea" placeholder="Add your text here"></textarea>
-        <br>
-        <div id="button">
-            <input id="submitButton" type="submit" value="Submit">
-        </div>
-    </form>
-
-    <section class="comment">
-        <div class='cloneComment'>
-            <img class="userImage" src="https://randomuser.me/api/portraits/men/29.jpg" alt="" style="width:60px">
-            <p class="commentText">Awesome !</p>
-        </div>
-    </section>
-
-    <hr>
-    <a href="#">Look for a mentor</a>
+<form id="form" action="">
+    @csrf
+    <label id="label" for="">Write a message</label>
     <br>
-    <a href="#">Modify profile</a>
+    <textarea name="" id="textArea" placeholder="Add your text here"></textarea>
+    <br>
+    <div id="button">
+        <input id="submitButton" type="submit" value="Submit">
+    </div>
+</form>
 
-    <!-- admin part -->
-    <hr>
-    @if(Auth::user()->type == 'mentor')
-    <a href="">Delete profile</a>
-    @endif
+<section class="comment">
+    <div class='cloneComment'>
+        <img class="userImage" src="https://randomuser.me/api/portraits/men/29.jpg" alt="" style="width:60px">
+        <p class="commentText">Awesome !</p>
+    </div>
+</section>
 
+<hr>
+<a href="#">Look for a mentor</a>
+<br>
+<a href="#">Modify profile</a>
 
-    <!-- mentor part -->
-    @if(Auth::user()->type == 'mentor')
-    <a href="">Accept invitation</a>
-    <a href="">Decline invitation</a>
-
-    <a href="">Disconnect</a>
-    @endif
-
-
-
-
-    <script>
-        const comments = [{
-                user: "mentor",
-                message: "Hello! My email address is asd@gmail.com"
-            },
-            {
-                user: "friend2",
-                message: "Amazing ! I can’t wait to work with you!"
-            },
-            {
-                user: "friend3",
-                message: "Wow! so Inspiring ! like my front end teacher !"
-            }
-        ]
+<!-- admin part -->
+<hr>
+@if(Auth::user()->type == 'mentor')
+<a href="">Delete profile</a>
+@endif
 
 
-        //clone comments 
+<!-- mentor part -->
+@if(Auth::user()->type == 'mentor')
+<a href="">Accept invitation</a>
+<a href="">Decline invitation</a>
 
-        const commentList = document.querySelector('.comment');
-        const commentClone = document.querySelector('.cloneComment');
+<a href="">Disconnect</a>
+@endif
 
-        for (const comment of comments) {
 
-            let newComment = commentClone.cloneNode(true);
 
-            newComment.querySelector('.userImage').src = "https://randomuser.me/api/portraits/men/1.jpg"
-            newComment.querySelector('.commentText').textContent = comment.message;
 
-            commentList.append(newComment);
+<script>
+    const comments = [{
+            user: "mentor",
+            message: "Hello! My email address is asd@gmail.com"
+        },
+        {
+            user: "friend2",
+            message: "Amazing ! I can’t wait to work with you!"
+        },
+        {
+            user: "friend3",
+            message: "Wow! so Inspiring ! like my front end teacher !"
         }
-        commentClone.remove();
+    ]
 
 
-        //add my comment
-        const myComment = document.getElementById('form');
-        myComment.addEventListener('submit', addNewComment);
+    //clone comments 
+
+    const commentList = document.querySelector('.comment');
+    const commentClone = document.querySelector('.cloneComment');
+
+    for (const comment of comments) {
+
+        let newComment = commentClone.cloneNode(true);
+
+        newComment.querySelector('.userImage').src = "https://randomuser.me/api/portraits/men/1.jpg"
+        newComment.querySelector('.commentText').textContent = comment.message;
+
+        commentList.append(newComment);
+    }
+    commentClone.remove();
 
 
-        function addNewComment(event) {
-            event.preventDefault();
-            let newCom = document.querySelector('#textArea').value;
-            comments.push(newCom);
+    //add my comment
+    const myComment = document.getElementById('form');
+    myComment.addEventListener('submit', addNewComment);
 
-            let writtenComment = commentClone.cloneNode(true);
-            writtenComment.querySelector('.commentText').textContent = newCom;
-            writtenComment.querySelector('.userImage').src = "{{asset('img/')}}/{{$profile->profile_image}}";
-            commentList.append(writtenComment);
-            //clean placeholder
-            document.getElementById('textArea').value = " ";
-        }
-    </script>
-</body>
 
-</html>
+    function addNewComment(event) {
+        event.preventDefault();
+        let newCom = document.querySelector('#textArea').value;
+        comments.push(newCom);
+
+        let writtenComment = commentClone.cloneNode(true);
+        writtenComment.querySelector('.commentText').textContent = newCom;
+        writtenComment.querySelector('.userImage').src = "{{asset('img/')}}/{{$profile->profile_image}}";
+        commentList.append(writtenComment);
+        //clean placeholder
+        document.getElementById('textArea').value = " ";
+    }
+</script>
+@endsection
