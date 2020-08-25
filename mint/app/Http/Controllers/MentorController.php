c03d47374734b625b8fbab13834e60271d71f539
jeyashreevaithiam <jeyashreevaithiam@users.noreply.github.com>
2020-08-25 12:21:19 +0200

    button jq - route

--- mint/app/Http/Controllers/MentorController.php
+++ mint/app/Http/Controllers/MentorController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
@@ -10,7 +11,7 @@ use App\User;
 class MentorController extends Controller
 {
     //
-  /**
+    /**
      * Display the specified resource.
      *
      * @param  int  $id
@@ -19,15 +20,15 @@ class MentorController extends Controller
     public function show($id)
     {
 
-     
-       // if(Auth::user()->type == 'mentor') 
-        
-            $mentor = User::find($id);
-            $skills = User::find($id)->skills;
-            $ratings = User::find($id)->receiveRatings;
-            
 
-            /*
+        // if(Auth::user()->type == 'mentor') 
+
+        $mentor = User::find($id);
+        $skills = User::find($id)->skills;
+        $ratings = User::find($id)->receiveRatings;
+
+
+        /*
             $mentee_info = DB::table('users')
             ->join('ratings', 'ratings.writer_id', '=', 'users.id')
             ->select('users.*')
@@ -35,56 +36,50 @@ class MentorController extends Controller
             ->get();
             */
 
-            $ratingExists = False;
-            //$ratingsWithName=collect([['check','5','check collect Comment']]);
-            $ratingsWithName=collect();
-            foreach($ratings as $rating)
-            {
-                // Create an array containing rater_name, rating, comments
-               // $rater = User::find($rating->writer_id);
+        $ratingExists = False;
+        //$ratingsWithName=collect([['check','5','check collect Comment']]);
+        $ratingsWithName = collect();
+        foreach ($ratings as $rating) {
+            // Create an array containing rater_name, rating, comments
+            // $rater = User::find($rating->writer_id);
 
-               $rater = User::find($rating->writer_id);
-               $raterName = $rater->firstname;
-               $ratingsWithName->push([$raterName,$rating->score,$rating->comment]);
+            $rater = User::find($rating->writer_id);
+            $raterName = $rater->firstname;
+            $ratingsWithName->push([$raterName, $rating->score, $rating->comment]);
 
-                
-                // Checking if the mentee has already rated the mentor 
-              if(($rating->writer_id == Auth::user()->id) && ($rating->target_id == $id))
-              {
-                  $ratingExists = True;
-              } 
+
+            // Checking if the mentee has already rated the mentor 
+            if (($rating->writer_id == Auth::user()->id) && ($rating->target_id == $id)) {
+                $ratingExists = True;
             }
+        }
 
-            //Check mentor availability
-            if($mentor->availability == 1)
-            {
-                $mentorAvailable = 'Yes';
-            }
-            else
-            {
-                $mentorAvailable = 'No';
-            }
-           // dd($ratingsWithName);
+        //Check mentor availability
+        if ($mentor->availability == 1) {
+            $mentorAvailable = 'Yes';
+        } else {
+            $mentorAvailable = 'No';
+        }
+        // dd($ratingsWithName);
 
-          //  dd($mentor->firstname); 
+        //dd($mentor->firstname);
         //dd($skills);
         //dd($ratings);
         //dd(Auth::user()->id);
-         //dd($ratingExists);
-            //return view('mentor', ['mentor' => $mentor],['skills' => $skills],['ratings' => $ratings]);   
-        return view('mentor', compact('mentor','skills','ratingExists','ratingsWithName','mentorAvailable'));       
-        }
+        //dd($ratingExists);
+        //return view('mentor', ['mentor' => $mentor],['skills' => $skills],['ratings' => $ratings]);   
+        return view('mentor', compact('mentor', 'skills', 'ratingExists', 'ratingsWithName', 'mentorAvailable'));
+    }
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-    
+
     public function create()
     {
-       
-    } 
-     
+    }
+
 
     /**
      * Store a newly created resource in storage.
@@ -92,18 +87,18 @@ class MentorController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-   
+
     //inserting score and comments into db table ratings
     public function store(Request $request)
     {
-       DB::table('ratings')->insert([
-            'writer_id' => $request->writer_id, 
+        DB::table('ratings')->insert([
+            'writer_id' => $request->writer_id,
             'target_id' => $request->target_id,
             'score' => $request->score,
-            'comment' => $request->comment, 
+            'comment' => $request->comment,
         ]);
-      //  return redirect("/mentorprofile/$request->id");
-    } 
+        //  return redirect("/mentorprofile/$request->id");
+    }
 
     /**
      * Show the form for editing the specified resource.
@@ -131,9 +126,18 @@ class MentorController extends Controller
         //
     }
 
+    public function destroy($id)
+    {
+        // $result = DB::delete('DELETE FROM users WHERE id = ? ', [$id]);
 
-    
 
+        $result = User::find($id);
+        $result->delete();
+        return response()->json(['message' => 'hellooooo' . $id]);
 
-    
+        // if ($result)
+        //     return response()->json(['message' => 'user(id:' . $id . ') destroyed']);
+        // else
+        //     return response()->json(['message' => 'Impossible to destroy the user with id : ' . $id]);
+    }
 }
--- mint/resources/views/mentor.blade.php
+++ mint/resources/views/mentor.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,10 +8,11 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>mentor</title>
 </head>
-<body>
-<!--Include the NAVBAR-->
 
-<!--Profile block
+<body>
+    <!--Include the NAVBAR-->
+
+    <!--Profile block
 
  Picture
  Firstname - lastname
@@ -29,11 +31,11 @@ Add rules for displaying the different functions depending of the viewer
  Add the form to have a new ratings
  Show the availability-->
 
- <section>
+    <section>
 
- 
- 
- </section>
+
+
+    </section>
 
 
     <img src="{{ asset('img/') }}/{{ $mentor->profile_image }}" width="400" height="300">
@@ -42,92 +44,163 @@ Add rules for displaying the different functions depending of the viewer
     <h1>Pitch : {{ $mentor->pitch }}</h1>
     <h1>linkedin : {{ $mentor->linkedin }}</h1>
     <h1>Available for mentorship : {{ $mentorAvailable }}</h1>
-   <!-- <label for="checkboxyes">Available for mentorship</label>
+    <!-- <label for="checkboxyes">Available for mentorship</label>
     <input type="checkbox" id="checkbox" name="available" value="{{ $mentor->availability}}"> -->
-    
+
     @foreach($skills as $skill)
-       <h1>{{$skill->skill}}</h1> 
+    <h1>{{$skill->skill}}</h1>
     @endforeach
 
-@if(Auth::user()->type == 'mentor')
-    <button>Edit the profile page</button>
-    <button>See all the invitation-request page</button>
-    <button>See all the connection page</button>
-@endif
+    @if(Auth::user()->type == 'mentor')
 
-@if(Auth::user()->type == 'admin')
+    <button class="editbtn" value="{{$mentor->id}}">Edit the profile page</button>
+    <button class="seeallinvitationbtn" value="{{$mentor->id}}">See all the invitation-request page</button>
+    <button class="seeallconnectionbtn" value="{{$mentor->id}}">See all the connection page</button>
 
-    <button>Delete the profile</button>
+    @endif
 
-@endif   
-@if(Auth::user()->type == 'mentee')
- 
- <button>Apply for the mentorship</button>
-    <h2>Ratings</h2>    
+    @if(Auth::user()->type == 'admin')
+
+    <button class="deletebtn" value="{{$mentor->id}}">Delete the profile</button>
+
+    @endif
+    @if(Auth::user()->type == 'mentee')
+
+
+    <button class="applymentorship" value="{{$mentor->id}}">Apply for the mentorship</button>
+
+
+    <h2>Ratings</h2>
     @foreach($ratingsWithName as $rating)
-        <h1>{{$rating[0]}}</h1> 
-        <h1>{{$rating[1]}}</h1> 
-        <h1>{{$rating[2]}}</h1> 
+    <h1>{{$rating[0]}}</h1>
+    <h1>{{$rating[1]}}</h1>
+    <h1>{{$rating[2]}}</h1>
     @endforeach
-  
-@if(!$ratingExists)
-    <form action="" method="POST">
+
+    @if(!$ratingExists)
+    <form action="" method=" POST">
         @csrf
-        <input name="writer_id" type="hidden" value="{{Auth::user()->id}}"/>
-        <input name="target_id" type="hidden" value="{{$mentor->id}}"/>
+        <input name="writer_id" type="hidden" value="{{Auth::user()->id}}" />
+        <input name="target_id" type="hidden" value="{{$mentor->id}}" />
         <textarea name="comment" id="comment" cols="30" rows="10"></textarea>
         <label for="rating">Ratings:</label>
-          <select id="rating" name="score">
+        <select id="rating" name="score">
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
             <option value="4">4</option>
             <option value="5">5</option>
-             </select>
-        <input type="submit" id="submit" value="submit">   
+        </select>
+        <input type="submit" id="submit" value="submit">
 
 
-     </form>
-     @endif
-@endif
-<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    </form>
+    @endif
+    @endif
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 
-<script type="text/javascript">
-
-   
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-    });
-
-    $("input[type="submit"]").click(function(e){
-
-        e.preventDefault();
-
-      
-        console.log(data);
-        
-
-        $.ajax({
-           //url:'/rating',
-           method:'POST',
-           data: $('form').serialize(),
-
-          success: function(result) {
-                console.log('data inserted successfully')
-			},
-
-            error: function(err){
-				// If ajax errors happens
-			}
-            
-            e.preventDefault();
+    <script type="text/javascript">
+        $("button[name='editbtn']").click(function(event) {
+            event.preventDefault();
+            routeUrl = "{{url('')}}/mentorprofile/edit" + $(this).val();
+            window.location.href = routeUrl;
         });
-	});
-</script>
+        $("button[name='seeallinvitationbtn']").click(function(event) {
+            event.preventDefault();
+            routeUrl = "{{url('')}}/mentorai/" + $(this).val();
+            window.location.href = routeUrl;
+        });
+        $("button[name='seeallconnectionbtn']").click(function(event) {
+            event.preventDefault();
+            routeUrl = "{{url('')}}/mentee/" + $(this).val();
+            window.location.href = routeUrl;
+        });
+        $("button[name='applymentorship']").click(function(event) {
+            event.preventDefault();
+            routeUrl = "{{url('')}}/mentorprofile/apply/" + $(this).val();
+            window.location.href = routeUrl;
+        });
 
 
-<!--Include the FOOTER-->  
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $("input[type='submit']").click(function(e) {
+
+            e.preventDefault();
+
+            $.ajax({
+                //url:'/rating',
+                method: 'POST',
+                data: $('form').serialize(),
+
+                success: function(result) {
+                    console.log('data inserted successfully')
+                },
+
+                error: function(err) {
+                    // If ajax errors happens
+                }
+
+
+            });
+        });
+    </script>
+
+
+    <script>
+        $(function() {
+            $('.deletebtn').click(function(e) {
+                let route = '/mentorprofile/delete/' + $(this).val();
+                console.log('Route: ' + route);
+                $.ajax({
+                    url: route,
+                    type: 'delete',
+                    dataType: 'json',
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    success: function(result) {
+                        console.log(result.message);
+
+                    },
+                    error: function(err) {
+
+                        alert('AJAX ERROR');
+                    }
+                });
+            });
+        });
+    </script>
+
+    <script>
+        $(function() {
+            $('.applybtn').click(function(e) {
+                let route = '/mentorprofile/apply/' + $(this).val();
+                console.log('Route: ' + route);
+                $.ajax({
+                    url: route,
+                    type: 'get',
+                    dataType: 'json',
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    success: function(result) {
+                        console.log(result.message);
+
+                    },
+                    error: function(err) {
+
+                        alert('AJAX ERROR');
+                    }
+                });
+            });
+        });
+    </script>
+
 </body>
+
 </html>
