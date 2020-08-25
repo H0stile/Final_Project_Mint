e6b98dfded2adbf264054ca690ba8d4c7aaf4ba6
AnnaKhitko <33467618+AnnaKhitko@users.noreply.github.com>
2020-08-25 16:23:27 +0200

    job API

--- mint/resources/views/mentee/jobs.blade.php
+++ mint/resources/views/mentee/jobs.blade.php
@@ -3,6 +3,7 @@
 
 @foreach($data['jobs'] as $job)
 <li>Job title: {{$job['title']}}</li>
-<li>Company name: {{$job['company_name']}}</li>
+<li>Company: {{$job['company_name']}}</li>
 <li><a href="{{$job['url']}}">Details</a></li>
+<hr>
 @endforeach
