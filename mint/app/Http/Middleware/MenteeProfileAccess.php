6adec83c47509bca0a906947db8b532b412d86de
AnnaKhitko <33467618+AnnaKhitko@users.noreply.github.com>
2020-08-25 16:19:26 +0200

    changes in the API

--- mint/app/Http/Middleware/MenteeProfileAccess.php
+++ mint/app/Http/Middleware/MenteeProfileAccess.php
@@ -21,6 +21,8 @@ class MenteeProfileAccess
     public function handle($request, Closure $next)
     {
         $loggedInUser = $request->user();
+        // $loggedInUser->mentors[0]->id;
+
         if ($loggedInUser->type === 'mentee' && $loggedInUser->id !== (int) $request->id) {
             return response()->view("unauthorized");
         }
--- mint/resources/views/mentee/jobs.blade.php
+++ mint/resources/views/mentee/jobs.blade.php
@@ -1,11 +1,8 @@
 <h1>Job list</h1>
 
-{{$data['jobs'][0]['title']}} //
-{{$data['jobs'][1]['title']}} //
-{{$data['jobs'][2]['title']}}
 
-<!-- foreach($outerArray as $key => $array) {
-foreach($array as $key1 => $value) {
-<li>$array[$key1]['sku_id']</li>
-}
-} -->
+@foreach($data['jobs'] as $job)
+<li>Job title: {{$job['title']}}</li>
+<li>Company name: {{$job['company_name']}}</li>
+<li><a href="{{$job['url']}}">Details</a></li>
+@endforeach
