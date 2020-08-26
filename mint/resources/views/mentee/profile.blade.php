e7d64dd212838ba05f3663b2e9cb6b47ae79ca0f
AnnaKhitko <33467618+AnnaKhitko@users.noreply.github.com>
2020-08-26 16:55:46 +0200

    Changes in script section

--- mint/resources/views/mentee/profile.blade.php
+++ mint/resources/views/mentee/profile.blade.php
@@ -155,63 +155,65 @@
 @endif
 
 @if(Auth::user()->type == 'mentor' || Auth::user()->type == 'mentee')
+@section('script')
+src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous">
+@endsection
 
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-<script>
-    $(document).ready(function() {
-        function deleteCollaboration() {
-            routeUrl = "{{route('mentor.connection.destroy', $collabRequestId)}}";
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                }
-            });
-            $.ajax({
-                url: routeUrl,
-                method: 'DELETE',
-                dataType: 'json',
-                success: function(result) {
-                    window.location.replace("{{route('mentorprofile', Auth::user()->id)}}");
-                }
-            })
-        }
+@section('script')
+$(document).ready(function() {
+function deleteCollaboration() {
+routeUrl = "{{route('mentor.connection.destroy', $collabRequestId)}}";
+$.ajaxSetup({
+headers: {
+'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+}
+});
+$.ajax({
+url: routeUrl,
+method: 'DELETE',
+dataType: 'json',
+success: function(result) {
+window.location.replace("{{route('mentorprofile', Auth::user()->id)}}");
+}
+})
+}
 
-        //? Button to accept invitation
-        $("button[name='accept-request']").click(function(event) {
-            event.preventDefault();
-            if (confirm("Are you sure to accept this invitation?")) {
-                routeUrl = "{{route('mentor.invitation.accept', $collabRequestId)}}";
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-                    }
-                });
-                $.ajax({
-                    url: routeUrl,
-                    method: 'GET',
-                    dataType: 'json',
-                    success: function(result) {
-                        location.reload();
-                    }
-                })
-            }
-        });
+//? Button to accept invitation
+$("button[name='accept-request']").click(function(event) {
+event.preventDefault();
+if (confirm("Are you sure to accept this invitation?")) {
+routeUrl = "{{route('mentor.invitation.accept', $collabRequestId)}}";
+$.ajaxSetup({
+headers: {
+'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+}
+});
+$.ajax({
+url: routeUrl,
+method: 'GET',
+dataType: 'json',
+success: function(result) {
+location.reload();
+}
+})
+}
+});
 
-        //? Button to decline collaboration request
-        $("button[name='decline-request']").click(function(event) {
-            event.preventDefault();
-            if (confirm("Are you sure to decline invitation?")) {
-                deleteCollaboration();
-            }
-        });
-        //? Button to break the connection
-        $("button[name='disconnect']").click(function(event) {
-            event.preventDefault();
-            if (confirm("Are you sure to disconnect from this mentee?")) {
-                deleteCollaboration();
-            }
-        });
-    });
-</script>
+//? Button to decline collaboration request
+$("button[name='decline-request']").click(function(event) {
+event.preventDefault();
+if (confirm("Are you sure to decline invitation?")) {
+deleteCollaboration();
+}
+});
+//? Button to break the connection
+$("button[name='disconnect']").click(function(event) {
+event.preventDefault();
+if (confirm("Are you sure to disconnect from this mentee?")) {
+deleteCollaboration();
+}
+});
+});
+@endsection
 @endif
 @endsection
