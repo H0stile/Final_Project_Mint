f54f7866866392fe10149bb4c744509fffc881eb
Matt <matthieu.barbie@gmail.com>
2020-08-27 10:24:47 +0200

    Update to have array in the where query and finish the name search

--- mint/app/Http/Controllers/searchmentorController.php
+++ mint/app/Http/Controllers/searchmentorController.php
@@ -109,13 +109,24 @@ class searchmentorController extends Controller
         $name = $request->name;
         //TODO USE A WHERE CONDITION USING ARRAY TO FACILITATE THE QUERY >> https://stackoverflow.com/questions/30706603/can-i-do-model-whereid-array-multiple-where-conditions/61722255#61722255
         if ($lang != null || $skill != null || $name != null) {
-            $mentorsData = DB::table('users')->join('skills_intermediate', 'skills_intermediate.user_id', '=', 'users.id')->join('skills', 'skills.id', '=', 'skills_intermediate.skill_id')->join('languages_intermediate', 'languages_intermediate.user_id', '=', 'users.id')->join('languages', 'languages.id', '=', 'languages_intermediate.language_id')->where('users.mentor_status', 'validate')->where('users.type', 'mentor')->where('languages', 'like', '%'.$lang.'%')->where('skill', 'like', '%'.$skill.'%')->get();
-            //* ->where('users.firstname', 'like', '=', '%'.$name.'%')->where('users.lastname', 'like', '=', '%'.$name.'%')
+            $conditions = array(
+                array('users.mentor_status', 'validate'),
+                array('users.type', 'mentor'),
+                array('languages', 'like', '%'.$lang.'%'),
+                array('skill', 'like', '%'.$skill.'%'),
+                array('lastname', 'like', '%'.$name.'%'),
+            );
+            $mentorsData = DB::table('users')->join('skills_intermediate', 'skills_intermediate.user_id', '=', 'users.id')->join('skills', 'skills.id', '=', 'skills_intermediate.skill_id')->join('languages_intermediate', 'languages_intermediate.user_id', '=', 'users.id')->join('languages', 'languages.id', '=', 'languages_intermediate.language_id')->where($conditions)->get();
             return response()->json([$mentorsData]);
             
             // return response()->json(['lang' => $lang, 'skill' => $skill, 'name' => $name ]);
         }else{
-            $mentorsData = DB::table('users')->join('skills_intermediate', 'skills_intermediate.user_id', '=', 'users.id')->join('skills', 'skills.id', '=', 'skills_intermediate.skill_id')->join('languages_intermediate', 'languages_intermediate.user_id', '=', 'users.id')->join('languages', 'languages.id', '=', 'languages_intermediate.language_id')->where('users.mentor_status', 'validate')->where('users.type', 'mentor')->get();
+            $conditions = array(
+                array('users.mentor_status', 'validate'),
+                array('users.type', 'mentor'),
+            );
+            $mentorsData = DB::table('users')->join('skills_intermediate', 'skills_intermediate.user_id', '=', 'users.id')->join('skills', 'skills.id', '=', 'skills_intermediate.skill_id')->join('languages_intermediate', 'languages_intermediate.user_id', '=', 'users.id')->join('languages', 'languages.id', '=', 'languages_intermediate.language_id')->where($conditions)->get();
+            //->where('users.type', 'mentor')
             return response()->json([$mentorsData]);
         }
     }
