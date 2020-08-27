c5af5fcc2b227369ef174e0f6253f8246b798d2e
Matt <matthieu.barbie@gmail.com>
2020-08-27 09:49:34 +0200

    Update app.blade.php

--- mint/resources/views/layouts/app.blade.php
+++ mint/resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>test</title>
     <!-- Styles -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
