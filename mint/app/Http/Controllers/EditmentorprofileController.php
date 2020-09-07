813eed6aff79ce85e1c3ab2125fbba783930e311
MattRM2 <matthieu.barbie@gmail.com>
2020-09-07 15:42:01 +0200

    Update EditmentorprofileController.php
    - Update to fill the user_fullName, user_languages and user_skills for engine search

--- mint/app/Http/Controllers/EditmentorprofileController.php
+++ mint/app/Http/Controllers/EditmentorprofileController.php
@@ -133,28 +133,43 @@ class EditmentorprofileController extends Controller
             'lastname' => $request->lastname,
             'linkedin' => $request->linkedin,
             'pitch' => $request->pitch,
-            'availability' => $availability
-
-
+            'availability' => $availability,
+            'user_fullName' => $request->firstname.' '.$request->lastname,
         ]);
 
         // dd($request->skillChkBox);
 
         // updating new mentor skills and languages
+        $skillsUser = "";
+        $langsUser = "";
         DB::table('skills_intermediate')->where('user_id', $id)->delete();
         foreach ($request->skillChkBox as $skillId) {
             DB::table('skills_intermediate')->insert([
                 'user_id' => $request->id,
                 'skill_id' => $skillId
             ]);
+            $currentSkill = Skill::where('id', $skillId)->first();
+            $skillsUser = $skillsUser.$currentSkill->skill.'-';
         }
+        //* Insert skills into users skills
+        DB::table('users')->where('id', $id)->update([
+            'user_skills' => '',
+            'user_skills' => $skillsUser,
+        ]);
         DB::table('languages_intermediate')->where('user_id', $id)->delete();
         foreach ($request->langChkBox as $langId) {
             DB::table('languages_intermediate')->insert([
                 'user_id' => $request->id,
                 'language_id' => $langId
             ]);
+            $currentLang = Language::where('id', $langId)->first();
+            $langsUser = $langsUser.$currentLang->languages.'-';
         }
+        //* Insert languages into users language
+        DB::table('users')->where('id', $id)->update([
+            'user_languages' => '',
+            'user_languages' => $langsUser,
+        ]);
         //dd($request->id);
         return redirect("/mentor/" . $request->id);
         //return redirect()->route('mentor', ['id' => $request->id]);
